09f97eb4daf36b4ce721f4e5179e9692ee169179
Kute <hrushi.kute@gmail.com>
2021-10-03 20:19:55 +0530

    Breadth first search

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+def insertionSort(input):
+    # compare the numbers and arrange accordingly
+    for i in range(0,len(input)):
+        for j in range(0,i):
+            
+
+
+
+a = [99,1,34,54,234,67,32,5]
+print(selectionSort(a))
